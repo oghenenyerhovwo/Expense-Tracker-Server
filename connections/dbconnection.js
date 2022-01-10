6c56629498b085a6f4c7e8ecc11f388943df255f
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-01-10 13:10:10 +0100

    Fixed Atlas db connection

--- connections/dbconnection.js
+++ connections/dbconnection.js
@@ -17,7 +17,7 @@ const databaseConnection = async () => {
     //Bind connection to error event (to get notification of connection errors)
     db.on("error", console.error.bind(console, "Connection error:"));
     db.once("open", () => {
-        console.log("Annunciation Database connected");
+        console.log("Database connected");
     });
 }
 
